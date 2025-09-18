5e05f8576c939d65ba520f85121d4e65d1afa0e0
Martin Vaca <mvacajr97@gmail.com>
2025-09-18 13:56:25 -0500

    module 2 completed baseline

--- app.js
+++ app.js
@@ -4,14 +4,17 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+//module2 changes
+var indexRouter = require('./app_server/routes/index');
+var usersRouter = require('./app_server/routes/users');
+var travelRouter = require('./app_server/routes/travel');
+var handlebars = require('hbs');
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+handlebars.registerPartials(__dirname + '/app_server/views/partials');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,8 +22,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//module2 changes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/travel', travelRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
